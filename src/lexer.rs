a47ed3aeac8be323b1923e5eed3966bb7c94e5c0
Daniel Klyus <d.klyus@g.nsu.ru>
2023-11-20 15:08:01 +0700

    Добавил обработку ошибок и новые тесты

--- src/lexer.rs
+++ src/lexer.rs
@@ -11,48 +11,104 @@ pub enum Token {
     Quote,
 }
 
-pub fn lex(program: &str) -> Vec<Token> {
+#[derive(PartialEq, Debug)]
+pub enum LexicalError {
+    UnexpectedEOF,
+    UnexpectedRParen,
+    UnclosedString,
+}
+
+pub fn lex(program: &str) -> Vec<Result<Token, LexicalError>> {
     let mut chars = program.chars().peekable();
-    let mut tokens: Vec<Token> = Vec::new();
+    let mut tokens: Vec<Result<Token, LexicalError>> = Vec::new();
+    let mut open_paren_count = 0;
+    let mut has_error = false;
     while let Some(&ch) = chars.peek() {
-        match ch {
+        if has_error {
+            break;
+        }
+        let result = match ch {
             '(' => {
-                tokens.push(Token::LParen);
+                open_paren_count += 1;
                 chars.next();
+                Ok(Token::LParen)
             }
             ')' => {
-                tokens.push(Token::RParen);
+                open_paren_count -= 1;
                 chars.next();
+                if open_paren_count < 0 {
+                    Err(LexicalError::UnexpectedRParen)
+                } else {
+                    Ok(Token::RParen)
+                }
             }
             '\'' => {
-                tokens.push(Token::Quote);
                 chars.next();
+                Ok(Token::Quote)
             }
-            whitespace if whitespace.is_whitespace() => {
+            _ if ch.is_whitespace() => {
                 chars.next();
+                continue;
             }
-            _ => {
-                tokens.push(finalize_token(&mut chars));
-            }
+            _ => finalize_token(&mut chars),
+        };
+        if let Err(_) = &result {
+            has_error = true;
         }
+        tokens.push(result);
     }
+
+    if open_paren_count > 0 && !has_error {
+        tokens.push(Err(LexicalError::UnexpectedEOF));
+    }
+
     tokens
 }
 
-fn finalize_token(chars: &mut Peekable<Chars>) -> Token {
+fn finalize_token(chars: &mut Peekable<Chars>) -> Result<Token, LexicalError> {
     let mut token_string = String::new();
+    let mut is_string = false;
     while let Some(&ch) = chars.peek() {
-        if ch.is_whitespace() || ch == '(' || ch == ')' || ch == '\'' {
+        if ch == '"' {
+            chars.next();
+            if is_string {
+                is_string = false;
+                break;
+            } else {
+                is_string = true;
+                continue;
+            }
+        }
+        if !is_string && (ch.is_whitespace() || ch == '(' || ch == ')' || ch == '\'') {
             break;
         }
-        token_string.push(ch);
+        if is_string && ch.is_whitespace() {
+            token_string.push(ch);
+            chars.next();
+            continue;
+        }
+        if is_string || (!is_string && !ch.is_whitespace()) {
+            token_string.push(ch);
+        }
         chars.next();
     }
-    token_string
-        .parse::<i64>()
-        .map(Token::Integer)
-        .or_else(|_| token_string.parse::<f64>().map(Token::Float))
-        .unwrap_or_else(|_| Token::String(token_string))
+    if is_string {
+        return Err(LexicalError::UnclosedString);
+    } else {
+        if token_string
+            .trim_start_matches('(')
+            .trim_end_matches(')')
+            .contains("\"")
+        {
+            return Err(LexicalError::UnclosedString);
+        }
+
+        Ok(token_string
+            .parse::<i64>()
+            .map(Token::Integer)
+            .or_else(|_| token_string.parse::<f64>().map(Token::Float))
+            .unwrap_or_else(|_| Token::String(token_string)))
+    }
 }
 
 #[cfg(test)]
@@ -60,49 +116,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lex_integer() {
-        let tokens = lex("(+ 1 2)");
-        assert_eq!(
-            tokens,
-            vec![
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::Integer(1),
-                Token::Integer(2),
-                Token::RParen,
-            ]
-        );
-    }
-
-    #[test]
-    fn test_lex_float() {
-        let tokens = lex("(+ 1.0 2.5)");
-        assert_eq!(
-            tokens,
-            vec![
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::Float(1.0),
-                Token::Float(2.5),
-                Token::RParen,
-            ]
-        );
-    }
-
-    #[test]
     fn test_lex_complex() {
         let tokens = lex("(cos (* 3.14159 1))");
         assert_eq!(
             tokens,
             vec![
-                Token::LParen,
-                Token::String("cos".to_string()),
-                Token::LParen,
-                Token::String("*".to_string()),
-                Token::Float(3.14159),
-                Token::Integer(1),
-                Token::RParen,
-                Token::RParen,
+                Ok(Token::LParen),
+                Ok(Token::String("cos".to_string())),
+                Ok(Token::LParen),
+                Ok(Token::String("*".to_string())),
+                Ok(Token::Float(3.14159)),
+                Ok(Token::Integer(1)),
+                Ok(Token::RParen),
+                Ok(Token::RParen),
             ]
         );
     }
@@ -113,60 +139,119 @@ mod tests {
         assert_eq!(
             tokens,
             vec![
-                Token::Quote,
-                Token::LParen,
-                Token::Integer(1),
-                Token::Integer(2),
-                Token::Integer(3),
-                Token::RParen,
+                Ok(Token::Quote),
+                Ok(Token::LParen),
+                Ok(Token::Integer(1)),
+                Ok(Token::Integer(2)),
+                Ok(Token::Integer(3)),
+                Ok(Token::RParen),
             ]
         );
     }
 
     #[test]
     fn test_lex_long() {
-        let tokens = lex("(+ (* 3
-            (+ (* 2 4)
-               (+ 3 5)
-            )
-         )
-         (+ (- 10 7)
-            6
-         )
-      )");
+        let tokens = lex("(+ (* 3 (+ (* 2 4)  (+ 3 5))) (+ (- 10 7) 6))");
         assert_eq!(
             tokens,
             vec![
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::LParen,
-                Token::String("*".to_string()),
-                Token::Integer(3),
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::LParen,
-                Token::String("*".to_string()),
-                Token::Integer(2),
-                Token::Integer(4),
-                Token::RParen,
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::Integer(3),
-                Token::Integer(5),
-                Token::RParen,
-                Token::RParen,
-                Token::RParen,
-                Token::LParen,
-                Token::String("+".to_string()),
-                Token::LParen,
-                Token::String("-".to_string()),
-                Token::Integer(10),
-                Token::Integer(7),
-                Token::RParen,
-                Token::Integer(6),
-                Token::RParen,
-                Token::RParen,
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::LParen),
+                Ok(Token::String("*".to_string())),
+                Ok(Token::Integer(3)),
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::LParen),
+                Ok(Token::String("*".to_string())),
+                Ok(Token::Integer(2)),
+                Ok(Token::Integer(4)),
+                Ok(Token::RParen),
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::Integer(3)),
+                Ok(Token::Integer(5)),
+                Ok(Token::RParen),
+                Ok(Token::RParen),
+                Ok(Token::RParen),
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::LParen),
+                Ok(Token::String("-".to_string())),
+                Ok(Token::Integer(10)),
+                Ok(Token::Integer(7)),
+                Ok(Token::RParen),
+                Ok(Token::Integer(6)),
+                Ok(Token::RParen),
+                Ok(Token::RParen),
             ]
+        );
+    }
+
+    #[test]
+    fn test_unexpected_eof() {
+        let result = lex("(+ 1 2");
+        assert_eq!(
+            result,
+            vec![
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::Integer(1)),
+                Ok(Token::Integer(2)),
+                Err(LexicalError::UnexpectedEOF)
+            ]
+        );
+    }
+
+    #[test]
+    fn test_string() {
+        let result = lex(r#"(display "Hello, world!")"#);
+        assert_eq!(
+            result,
+            vec![
+                Ok(Token::LParen),
+                Ok(Token::String("display".to_string())),
+                Ok(Token::String("Hello, world!".to_string())),
+                Ok(Token::RParen),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_unclosed_string() {
+        let result = lex(r#"(display "hello)"#);
+        assert_eq!(
+            result,
+            vec![
+                Ok(Token::LParen),
+                Ok(Token::String("display".to_string())),
+                Err(LexicalError::UnclosedString),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_unexpected_close_paren() {
+        let result = lex("(+ 1 2))");
+        assert_eq!(
+            result,
+            vec![
+                Ok(Token::LParen),
+                Ok(Token::String("+".to_string())),
+                Ok(Token::Integer(1)),
+                Ok(Token::Integer(2)),
+                Ok(Token::RParen),
+                Err(LexicalError::UnexpectedRParen),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_void() {
+        let result = lex("");
+        assert_eq!(
+            result,
+            vec![]
         );
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -8,67 +8,53 @@ pub enum Expr {
     String(String),
     List(Vec<Expr>),
     Quote(Box<Expr>),
+    Void
 }
 
-pub fn parse(tokens: &[Token]) -> Result<(Expr, &[Token]), String> {
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    LexError(LexicalError),
+    UnexpectedToken,
+}
+
+pub fn parse(tokens: &[Result<Token, LexicalError>]) -> Result<(Expr, &[Result<Token, LexicalError>]), ParseError> {
+    if tokens.is_empty() {
+        return Ok((Expr::Void, tokens));
+    }
     let (item, rest) = tokens.split_at(1);
     match item {
-        [Token::Integer(i)] => Ok((Expr::Integer(*i), rest)),
-        [Token::Float(f)] => Ok((Expr::Float(*f), rest)),
-        [Token::String(s)] => Ok((Expr::String(s.clone()), rest)),
-        [Token::LParen] => {
+        [Ok(Token::Integer(i))] => Ok((Expr::Integer(*i), rest)),
+        [Ok(Token::Float(f))] => Ok((Expr::Float(*f), rest)),
+        [Ok(Token::String(s))] => Ok((Expr::String(s.clone()), rest)),
+        [Ok(Token::LParen)] => {
             let mut sub_exprs = vec![];
             let mut rem = rest;
-            while !matches!(rem.first(), Some(Token::RParen) | None) {
+            while !matches!(rem.first(), Some(Ok(Token::RParen)) | None) {
                 let (expr, remaining) = parse(rem)?;
                 sub_exprs.push(expr);
                 rem = remaining;
             }
-            if let [Token::RParen] = rem.split_at(1).0 {
+            if let [Ok(Token::RParen)] = rem.split_at(1).0 {
                 Ok((Expr::List(sub_exprs), &rem[1..]))
             } else {
-                Err("Unmatched left parenthesis".to_string())
+                Err(ParseError::LexError(LexicalError::UnexpectedEOF))
             }
         }
-        [Token::Quote] => {
+        [Ok(Token::Quote)] => {
             let (expr, remaining) = parse(rest)?;
             Ok((Expr::Quote(Box::new(expr)), remaining))
         }
-        _ => Err("Unexpected end of input".to_string()),
+        [Err(LexicalError::UnexpectedEOF)] => Err(ParseError::LexError(LexicalError::UnexpectedEOF)),
+        [Err(LexicalError::UnclosedString)] => Err(ParseError::LexError(LexicalError::UnclosedString)),
+        [Err(LexicalError::UnexpectedRParen)] => Err(ParseError::LexError(LexicalError::UnexpectedRParen)),
+        _ => Err(ParseError::UnexpectedToken)
     }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_parse_int() {
-        let tokens = lex("(+ 1 2)");
-        let parsed = parse(&tokens).unwrap().0;
-        assert_eq!(
-            parsed,
-            Expr::List(vec![
-                Expr::String("+".to_string()),
-                Expr::Integer(1),
-                Expr::Integer(2),
-            ])
-        );
-    }
-
-    #[test]
-    fn test_parse_float() {
-        let tokens = lex("(+ 1.0 2.5)");
-        let parsed = parse(&tokens).unwrap().0;
-        assert_eq!(
-            parsed,
-            Expr::List(vec![
-                Expr::String("+".to_string()),
-                Expr::Float(1.0),
-                Expr::Float(2.5),
-            ])
-        );
-    }
 
     #[test]
     fn test_parse_complex() {
@@ -144,6 +130,67 @@ mod tests {
                     Expr::Integer(6),
                 ]),
             ])
+        );
+    }
+
+    #[test]
+    fn test_unexpected_eof() {
+        let tokens = lex("(+ 1 2");
+        let parsed = parse(&tokens);
+        assert_eq!(
+            parsed,
+            Err(ParseError::LexError(LexicalError::UnexpectedEOF))
+        );
+    }
+
+    #[test]
+    fn test_string() {
+        let tokens = lex(r#"(display "Hello, world!")"#);
+        let parsed = parse(&tokens).unwrap().0;
+        assert_eq!(
+            parsed,
+            Expr::List(vec![
+                Expr::String("display".to_string()),
+                Expr::String("Hello, world!".to_string()),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_unclosed_string() {
+        let tokens = lex(r#"(display "hello)"#);
+        let parsed = parse(&tokens);
+        assert_eq!(
+            parsed,
+            Err(ParseError::LexError(LexicalError::UnclosedString))
+        );
+    }
+
+    #[test]
+    fn test_unexpected_close_paren() {
+        let tokens = lex("(+ 1 2))");
+        let (parsed, remaining) = parse(&tokens).unwrap();
+        assert_eq!(
+            parsed,
+            Expr::List(vec![
+                Expr::String("+".to_string()),
+                Expr::Integer(1),
+                Expr::Integer(2),
+            ])
+        );
+        assert_eq!(
+            remaining.first(),
+            Some(&Err(LexicalError::UnexpectedRParen))
+        );
+    }
+
+    #[test]
+    fn test_void() {
+        let tokens = lex("");
+        let parsed = parse(&tokens).unwrap().0;
+        assert_eq!(
+            parsed,
+            Expr::Void
         );
     }
 }
