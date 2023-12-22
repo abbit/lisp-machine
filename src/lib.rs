b8d290af529a4111755e27062aeb2bcc81946748
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2023-12-22 09:37:27 +0700

    add minimal docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
+//! LispDM is a Scheme interpreter. Currently, it supports only a subset of Scheme.
+//! ```
+//! use lispdm::Engine;
+//! let mut engine = Engine::default();
+//!
+//! let result = engine.eval("(+ 1 2)").unwrap();
+//! assert_eq!(result, lispdm::Expr::Integer(3));
+//! ```
 mod evaluator;
 mod expr;
 mod parser;
 mod utils;
 
 use evaluator::{EnvRef, EvalError};
+pub use expr::Expr;
 use parser::ParseError;
 
 #[derive(Debug)]
+/// Error type for LispDM.
 pub enum LispDMError {
+    /// Error occurred while parsing source code.
     ParseError(ParseError),
+    /// Error occurred while evaluating expressions.
     EvalError(EvalError),
 }
 
@@ -23,11 +35,14 @@ impl std::fmt::Display for LispDMError {
     }
 }
 
+/// Engine provides the main functionality of LispDM.
+/// It holds the environment and provides eval method.
 pub struct Engine {
     root_env: EnvRef,
 }
 
 impl Engine {
+    /// Evaluates the given source code and returns the result.
     pub fn eval(&mut self, src: &str) -> Result<expr::Expr, LispDMError> {
         let ast = parser::parse_str(src).map_err(LispDMError::ParseError)?;
         evaluator::eval_exprs(ast.into_iter(), &mut self.root_env).map_err(LispDMError::EvalError)
