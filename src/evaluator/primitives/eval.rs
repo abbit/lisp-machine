38f863682aef9fae9fc868894690dd92328456f8
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2024-01-07 08:08:55 +0700

    implement apply with more than 2 params

--- src/evaluator/primitives/eval.rs
+++ src/evaluator/primitives/eval.rs
@@ -5,7 +5,7 @@ use crate::{
 };
 
 define_procedures! {
-    apply = ("apply", apply_fn, Arity::Exact(2)),
+    apply = ("apply", apply_fn, Arity::AtLeast(2)),
     eval = ("eval", eval_fn, Arity::Exact(1)),
 }
 
@@ -16,14 +16,16 @@ fn apply_fn(mut args: Exprs, env: &mut EnvRef) -> ProcedureResult {
             expr.kind()
         )
     })?;
-    let operands = args.pop_front().unwrap().into_list().map_err(|expr| {
+    let last_list = args.pop_back().unwrap().into_list().map_err(|expr| {
         runtime_error!(
             "expected list as second argument for apply, got {}",
             expr.kind()
         )
     })?;
 
-    proc.apply(operands.into(), env)
+    args.extend(last_list);
+
+    proc.apply(args, env)
 }
 
 fn eval_fn(mut args: Exprs, env: &mut EnvRef) -> ProcedureResult {
