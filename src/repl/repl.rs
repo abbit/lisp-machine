7b900c9e4145418d6146db107f5977b6443676f1
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2024-04-07 15:54:57 +0700

    use file history for repl

--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -1,5 +1,8 @@
+use std::ops::Deref;
+use std::path::Path;
+
 use lispdm::{Engine, Expr};
-use rustyline::history::MemHistory;
+use rustyline::history::FileHistory;
 use rustyline::{error::ReadlineError, validate::MatchingBracketValidator};
 use rustyline::{highlight::MatchingBracketHighlighter, Editor};
 use rustyline::{Completer, CompletionType, Helper, Highlighter, Hinter, Validator};
@@ -16,14 +19,21 @@ pub fn start(mut engine: Engine) {
     println!("LispDM v0.0.1");
     println!("Use (exit), or Ctrl-D to exit REPL");
 
+    let expanded_path = shellexpand::tilde("~/.lispdm_history");
+    let history_path = Path::new(expanded_path.deref());
+    if !history_path.exists() {
+        std::fs::File::create(history_path).expect("Failed to create history file");
+    }
     let mut rl = Editor::with_history(
         rustyline::config::Config::builder()
             .auto_add_history(true)
             .completion_type(CompletionType::List)
             .build(),
-        MemHistory::default(),
+        FileHistory::new(),
     )
     .expect("Failed to instantiate REPL!");
+    rl.load_history(&history_path)
+        .expect("Failed to load history");
     rl.set_helper(Some(InputValidator::default()));
 
     loop {
@@ -33,6 +43,8 @@ pub fn start(mut engine: Engine) {
                 if input.trim().is_empty() {
                     continue;
                 }
+                rl.save_history(&history_path)
+                    .expect("Failed to save history");
                 match engine.eval::<Expr>(&input) {
                     Ok(expr) => {
                         println!("{}", expr.unwrap());
